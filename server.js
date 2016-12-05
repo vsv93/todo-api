4907039edc3197916fed1cee0c24eb9f0c153f40
Venkat <vsv_93@yahoo.com>
2016-12-06 00:53:53 +0530

    todo model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextID = 1;
@@ -47,13 +48,19 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
-		return res.status(400).send();
-	}
-	body.description = body.description.trim();
-	body.id = todoNextID++;
-	todos.push(body);
-	res.json(body);
+
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+	// 	return res.status(400).send();
+	// }
+	// body.description = body.description.trim();
+	// body.id = todoNextID++;
+	// todos.push(body);
+	// res.json(body);
 });
 
 app.put('/todos/:id', function(req, res) {
@@ -94,7 +101,8 @@ app.delete('/todos/:id', function(req, res) {
 	res.json(todoObject);
 })
 
-
-app.listen(PORT, function() {
-	console.log('Server started at port: ' + PORT);
-})
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Server started at port: ' + PORT);
+	});
+});
