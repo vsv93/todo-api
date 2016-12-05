fe2bc17a18517ea74b9b6f7ac792ed329c667d31
Venkat <vsv_93@yahoo.com>
2016-12-06 01:26:28 +0530

    goto todo api

--- server.js
+++ server.js
@@ -13,25 +13,24 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	var query = req.query;
+	var where = {};
+	if (query.hasOwnProperty('completed') && query.completed == 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed == 'false') {
+		where.completed = false;
 	}
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.indexOf(queryParams.q) > -1;
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0){
+		where.description={
+			$like : '%'+query.q+'%'
+		}
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+	}, function(e){
+		res.status(500).send();
+	});
 });
 
 app.get('/todos/:id', function(req, res) {
