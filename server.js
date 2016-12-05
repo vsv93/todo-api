639e58ae256a93255640b18bd266d140f2d8a01e
Venkat <vsv_93@yahoo.com>
2016-12-05 15:01:15 +0530

    body parser added

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var todos =[{
-	id: 1,
-	description: 'Learn node.js',
-	completed: false
-},{
-	id: 2,
-	description: 'Finish todo app',
-	completed: false
-},{
-	id: 3,
-	description: 'Buy groceries',
-	completed: false
-}];
+var todos =[];
+var todoNextID = 1;
 
+app.use(bodyParser.json());
 app.get('/', function(req, res){
 	res.send('Todo API root!');
 });
@@ -36,6 +27,13 @@ app.get('/todos/:id', function(req,res){
 	}else{
 		res.status(404).send('ID does not exist!!');
 	}
+});
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+	body.id = todoNextID++;
+	todos.push(body);	
+	res.json(body);
 });
 app.listen(PORT, function(){
 	console.log('Server started at port: '+PORT);
