9747651182dbfc316366492e331486b40d3de5fb
Venkat <vsv_93@yahoo.com>
2016-12-06 09:32:54 +0530

    postgres added changed

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-var Sequelize = require('Sequelize');
+var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 if (env == 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
-	})
+	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
