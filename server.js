f21f1599a6798574a776d88aa5025b2adf34b55d
Venkat <vsv_93@yahoo.com>
2016-12-06 10:30:35 +0530

    put sequelize

--- server.js
+++ server.js
@@ -20,28 +20,30 @@ app.get('/todos', function(req, res) {
 	} else if (query.hasOwnProperty('completed') && query.completed == 'false') {
 		where.completed = false;
 	}
-	if (query.hasOwnProperty('q') && query.q.length > 0){
-		where.description={
-			$like : '%'+query.q+'%'
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
 		}
 	}
 
-	db.todo.findAll({where: where}).then(function(todos){
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	}, function(e){
+	}, function(e) {
 		res.status(500).send();
 	});
 });
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	db.todo.findById(todoId).then(function(todo){
-		if(!!todo){
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
 			res.json(todo.toJSON());
-		}else{
+		} else {
 			res.status(404).send('ID doesnt exist!');
 		}
-	}, function(e){
+	}, function(e) {
 		res.status(500).json(e);
 	});
 });
@@ -49,52 +51,55 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function(todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function(e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
 
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttrib = {};
+	var attributes = {};
 	var todoId = parseInt(req.params.id, 10);
-	var todoObject = _.findWhere(todos, {
-		id: todoId
-	});
-	if (!todoObject) {
-		return res.status(404).send();
+
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttrib.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttrib.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		res.status(400).send();
-	}
-	_.extend(todoObject, validAttrib);
-	res.json(todoObject);
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			})
+		} else {
+			res.status(404).send('ID doesnt exist');
+		}
+	}, function() {
+		res.status(500).send();
+	})
 });
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	db.todo.destroy({
-		where:{
+		where: {
 			id: todoId
 		}
-	}).then(function(rowsDeleted){
-		if(rowsDeleted == 0){
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted == 0) {
 			res.status(404).send('ID doesnt exist!');
-		}else{
+		} else {
 			res.status(204).send();
 		}
-	}, function(){
+	}, function() {
 		res.status(500).send();
 	})
 })
