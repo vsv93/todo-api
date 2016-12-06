0349f169cc494302302211d676c127e68e12df8d
Venkat <vsv_93@yahoo.com>
2016-12-06 09:27:19 +0530

    postgres added

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
-var Sequelize =  require('Sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect' : 'sqlite',
-	'storage' : __dirname+ '/data/dev-todo-api.sqlite'
-});
+var Sequelize = require('Sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if (env == 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	})
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
 
 var db = {};
 
