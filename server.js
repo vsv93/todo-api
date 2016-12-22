4b1f8ab969e1987985fa5d0efaf565f01b1a7056
Venkat <vsv_93@yahoo.com>
2016-12-22 11:18:43 +0530

    add post user request

--- server.js
+++ server.js
@@ -104,6 +104,18 @@ app.delete('/todos/:id', function(req, res) {
 	})
 })
 
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user){
+		res.json(user.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
+
+});
+
+//Add Users
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Server started at port: ' + PORT);
