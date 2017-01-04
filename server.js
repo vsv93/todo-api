42b6693071523dcd84ad39c9674bcb61207d5fd7
Venkat <vsv_93@yahoo.com>
2017-01-04 15:24:21 +0530

    add login route and authentication

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextID = 1;
@@ -116,7 +117,17 @@ app.post('/users', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user){
+		res.json(user.toPublicJSON());
+	}, function(e){
+		res.status(401).send();
+	});
+});
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Server started at port: ' + PORT);
 	});
