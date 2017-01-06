d50f68a4d64e594962e9f5ab0d0e7fa4c5cb40ba
Venkat <vsv_93@yahoo.com>
2017-01-06 10:58:04 +0530

    adding token functionality

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -66,6 +68,23 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
+			},
+			generateToken: function(type){
+				if(!_.isString(type)){
+					return undefined;
+				}
+				try {
+					var stringData  = JSON.stringify({id: this.get('id'), type : type});
+					var encryptedData =  cryptojs.AES.encrypt(stringData, 'abc@123!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'xyz@123!');
+
+					return token;
+				} catch(e){
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON());
+		res.header('Auth', user.generateToken('Authentication')).json(user.toPublicJSON());
 	}, function(e){
 		res.status(401).send();
 	});
