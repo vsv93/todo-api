e052dd1f1756a53b13ac1306b9cb169be6500f37
Venkat <vsv_93@yahoo.com>
2017-01-06 14:17:48 +0530

    add associations for todo

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/', function(req, res) {
 
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId : req.user.get('id')
+	};
 	if (query.hasOwnProperty('completed') && query.completed == 'true') {
 		where.completed = true;
 	} else if (query.hasOwnProperty('completed') && query.completed == 'false') {
@@ -40,7 +42,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id : todoId,
+			userId : req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -79,7 +86,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id : todoId,
+			userId : req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -98,6 +110,7 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	db.todo.destroy({
 		where: {
+			userId : req.user.get('id'),
 			id: todoId
 		}
 	}).then(function(rowsDeleted) {
